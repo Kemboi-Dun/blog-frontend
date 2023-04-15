cb186585d0ff19ef1940c4aaccb3f743f5b59009
Kemboi-Dun <gahrmicc@gmail.com>
2023-04-16 01:40:35 +0300

    Large screens navbar ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className='nav_container'>
+    <div className="nav_container">
       <div className="nav_wrapper">
-<nav>
-  <div className="logo">
-    <h3>
-      Uncover
-    </h3>
-  </div>
-  <div className="links">
-    <Link className='link' to="/?cat=technology">Technology</Link>
-    <Link className='link' to="/?cat=art">Art</Link>
-    <Link className='link' to="/?cat=music">Music</Link>
-    <Link className='link' to="/?cat=food">Food</Link>
-    <span>
-      <p>
-        John
-      </p>
-      <Link className='link' to="/">Logout</Link>
-    </span>
-<div className="write">
-<Link className='link' to="/write">Write</Link>
-</div>
-  </div>
-</nav>
+        <nav>
+          <div className="logo">
+            <Link className="link"  to="/">
+            <h3>Uncover</h3>
+            </Link>
+            <div className="logo_dot"></div>
+          </div>
+          <div className="links">
+            <Link className="link" to="/?cat=technology">
+              <h6>
+              Technology
+              </h6>
+            </Link>
+            <Link className="link" to="/?cat=art">
+              <h6>Art</h6>
+            </Link>
+            <Link className="link" to="/?cat=music">
+              <h6>Music</h6>
+            </Link>
+            <Link className="link" to="/?cat=food">
+              <h6>Food</h6>
+            </Link>
+            <span>
+              <p>John</p>
+              <Link className="link" to="/">
+                Logout
+              </Link>
+            </span>
+            <div className="write">
+              <Link className="link" to="/write">
+                Write
+              </Link>
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
