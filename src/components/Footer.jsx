18fc503a26b716754e9ce6b3904d44cc231b8f04
Kemboi-Dun <gahrmicc@gmail.com>
2023-04-16 01:51:20 +0300

    Footer ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Footer() {
+  const date = new Date().getFullYear();
   return (
-    <div>Footer</div>
+    <div className='footer_container'>
+      <div className="footer_wrapper">
+<div className="copyright">
+  Copyright.&copy;{date}
+</div>
+<div className="privacy">
+  <Link className="link" to="/">
+  Privacy and Policy.
+  </Link>
+</div>
+      </div>
+    </div>
   )
 }
 
